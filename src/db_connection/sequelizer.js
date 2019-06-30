a71f29b7daa68bfe35745a2831dcabd5faa8500e
shin donggyu <donggyu9410@gmail.com>
2019-06-30 15:44:42 +0900

    Init config done

---
+++ src/db_connection/sequelizer.js
@@ -0,0 +1,19 @@
+import Sequelize from 'sequelize'
+
+const sequelize = new Sequelize('finchain', 'root', '1234', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+})
+
+sequelize.authenticate().then(() => {
+    console.log("Connection has been established successfully")
+}).catch(err => {
+    console.log("Unable to connect to the database: ", err)
+})
+
--- src/web.js
+++ src/web.js
@@ -1,4 +1,5 @@
 import App from "./app";
+import './db_connection'
 const port = 3000
 
 App.listen(port, () => console.log(`Example app listening on port ${port}`))
